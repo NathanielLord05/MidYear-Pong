16ba73f3ce45dcb9bd63bd6536d156848a1096c2
Nathaniel Lord <44natel@gmail.com>
2021-02-05 09:10:32 -0500

    started coding paddles

--- paddles.py
+++ paddles.py
@@ -1,1 +1,14 @@
 #Nate
+
+import pygame
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, height, width, color):
+        super().__init__()
+
+        self.paddle = pygame.pygame.Surface(width, height)
+        self.paddle.fill(0, 0, 0)
+        self.paddle.set_colorkey(0, 0, 0)
+
+        pygame.draw.rect(self.paddle, color, ([Black, width, height])
+        
