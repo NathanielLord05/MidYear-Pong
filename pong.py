72ea9cdb7b43a201680425016762fc1ea4fa38bc
sara aggarwal <sarajungkookbts@gmail.comT>
2021-02-10 08:38:57 -0500

    main file, a quarter way done

--- pong.py
+++ pong.py
@@ -1,1 +1,15 @@
-#Sara
+#sara 
+# creating main screen
+screen = ---.Screen()
+screen.title = ("Play the Pong Game!")
+screen.setup(width=900, height = 500)
+screen.bgcolor = ("black")
+
+# right paddle
+r_paddle = ()
+r_paddle.speed(0)
+r_paddle.shape("triangle")
+r_paddle.color("white")
+r_paddle.shapesize(stretch_wid=5, stretch_len=2)
+
+
